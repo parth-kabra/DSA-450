da82df54bd53607a5a38b323e1bdbccb2d1e7dda
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-05-20 19:22:58 +0530

    Create Print all subsequences of a string.cpp

---
+++ Strings/Print all subsequences of a string.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+#define str string
+#define ff first
+#define vec vector
+#define naturalNumSum(N) N*(N+1)/2
+#define p(x) push(x)
+const int mod=1e9+7;
+#define gcd(a,b) __gcd(a,b);
+#define lower(string) tolower(string)
+#define lcm(a,b) a*b/gcd(a,b);
+#define pi pair
+#define ss second
+#define tc(t) while(t--)
+#define pb(a) push_back(a);
+#define endl "\n"
+#define db double
+#define clr(a) a.clear();
+#define fl float
+#define del delete
+#define umap unordered_map
+#define mp make_pair
+#define ins	insert
+#define stu struct
+#define vd void
+#define null NULL
+#define cin(a) cin>>a;
+#define input(v,n) for(int i=0;i<n;i++){int m;cin(m);v.pb(m);}
+#define ppf	pop_front
+#define elif else if
+#define stk stack
+#define len(string)	string.length()
+#define print(a) cout<<a<<endl;
+#define lb lower_bound
+#define ub upper_bound
+#define emp(s) s.empty()
+#define printArr(arr) for(auto &i : arr){cout<<i<<" ";}
+#define printMap(m)	for(auto &v : m){cout<<v.ff<<" "<<v.ss<<endl;}
+vd Sub(str s, str o){
+	if(emp(s)){
+		cout<<o<<", ";
+		return;
+	}
+	Sub(s.substr(1), o + s[0]);
+	Sub(s.substr(1), o);
+}
+vd solve(){
+	int t;cin>>t;
+	tc(t){
+		str s;cin>>s;
+		str o="";
+		Sub(s,o);
+	}
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+   	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	   solve();
+	return 0;
+}

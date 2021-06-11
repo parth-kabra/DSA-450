8b299ede29f13fa47b6cc982c31d4c6887ebfff1
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-11 18:24:17 +0530

    Create Intersection Point of two Linked Lists.cpp

---
+++ Linked List/Intersection Point of two Linked Lists.cpp
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    if(head1==NULL || head2==NULL){
+        return -1;
+    }
+    Node * a=head1;
+    Node * b=head2;
+    
+    while(a!=b){
+        a=a==NULL?head2:a->next;
+        b=b==NULL?head1:b->next;
+    }
+    return a->data;
+}
+
